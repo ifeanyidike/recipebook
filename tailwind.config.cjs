734bb795f4f3c7b864dfc5cbee5b2831ac66be35
Ifeanyi Dike <ifeanyi.dike@tryarbor.com>
2024-09-13 16:36:29 +0100

    configure loader animation

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -5,7 +5,27 @@ module.exports = {
     "./src/**/*.{html,js,svelte,ts}"  // Include TypeScript files
   ],
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        'rotate-loader-1': 'loader1Rotate 2s infinite'
+      },
+      keyframes: {
+        loader1Rotate: {
+          '0%': {
+            '-webkit-transform': 'rotate(0deg) scale(0.8)',
+            '-moz-transform': 'rotate(0deg) scale(0.8)',
+          },
+          '50%': {
+            '-webkit-transform': 'rotate(360deg) scale(1.2)',
+            '-moz-transform': 'rotate(360deg) scale(1.2)',
+          },
+          '100%': {
+            '-webkit-transform': 'rotate(720deg) scale(0.8)',
+            '-moz-transform': 'rotate(720deg) scale(0.8)',
+          },
+        },
+      }
+    },
   },
   plugins: [],
 }
